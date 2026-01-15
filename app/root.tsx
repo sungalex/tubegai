dcb488d4da6834f287dca1dec98081c5a1d21a1a
sungalex <cloudallera@gmail.com>
2026-01-16 04:26:36 +0900

    브라우저 확장 프로그램으로 인한 콘솔 에러 메시지 Suppress

--- app/root.tsx
+++ app/root.tsx
@@ -39,7 +39,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <Meta />
         <Links />
       </head>
-      <body>
+      <body suppressHydrationWarning>
         {children}
         <Toaster />
         <ScrollRestoration />
